d402a38aac8aca20e95400fcce481e9878e5b9c7
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-30 17:56:59 +0700

    input elemen array dan penampilan header input

--- Bbinarysearch.cpp
+++ Bbinarysearch.cpp
@@ -20,4 +20,13 @@ void input ()
         }
 
     }
+
+    cout << "\n==================================\n";
+    cout << "       Masukkan elemen Array       \n";
+    cout << "===================================\n";
+    for (int i = 0; i < nPanjang; i++)
+    {
+    cout  << "Data ke-" <<  (i + 1) <<  "=";
+    cin >> element[i];
+    }
 }
